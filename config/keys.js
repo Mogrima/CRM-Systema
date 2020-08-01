53ae2059d52a1a01255c37ad17397449053dee4c
Mogrima <Rebel.RS@yandex.ru>
2020-08-01 23:25:47 +0500

    сделана авторизация в систему

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb+srv://popaze98:popaze98@cluster0.tcgvf.mongodb.net/<dbname>?retryWrites=true&w=majority'
+  mongoURI: 'mongodb+srv://popaze98:popaze98@cluster0.tcgvf.mongodb.net/<dbname>?retryWrites=true&w=majority',
+  jwt: 'dev-jwt'
 }
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,37 @@
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const User = require('../models/User')
+const keys = require('../config/keys')
 
-module.exports.login = function(req, res) {
-  res.status(200).json({
-    login: {
-      email: req.body.email,
-      password: req.body.password
+module.exports.login = async function(req, res) {
+  // проверяет, существует ли пользователь (email)
+  const candidate = await User.findOne({email: req.body.email})
+  // пользователь существует, проверка пароля
+  if (candidate) {
+    const passwordResult = bcrypt.compareSync(req.body.password, candidate.password)
+    if (passwordResult) {
+      // Генерация токена, пароль верный
+
+      const token = jwt.sign({
+        email: candidate.email,
+        userId: candidate._id
+      }, keys.jwt, {expiresIn: 3600})
+      res.status(200).json({
+        token: `Bearer ${token}`
+      })
     }
-  })
+    else {
+      res.status(401).json({
+        message: 'Пароли не совпадают. Попробуйте снова!'
+      })
+    }
+  } 
+  // Пользователя нет
+  else {
+    res.status(404).json({
+      message: 'Пользователь с таким email не найден.'
+    })
+  }
 }
 
 module.exports.register = async function(req, res) {
