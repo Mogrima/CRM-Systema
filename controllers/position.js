15a8a0b00b08f5c7897797ccac4ecb9533cae3e0
Mogrima <Rebel.RS@yandex.ru>
2020-08-04 16:31:08 +0500

    добавлен вывод всех позиций в контроллере position

--- controllers/position.js
+++ controllers/position.js
@@ -1,7 +1,38 @@
-module.exports.getByCategoryId = function(req, res) {}
+const Position = require('../models/Position')
+const errorHandler = require('../utils/errorHandler')
 
-module.exports.create = function(req, res) {}
+module.exports.getByCategoryId = async function(req, res) {
+  try {
+    const positions = await Position.find({
+      category: req.params.categoryId,
+      user: req.user.id
+    })
+    res.status(200).json(positions)
+  } catch (error) {
+    errorHandler(res, error)
+  }
+}
 
-module.exports.remove = function(req, res) {}
+module.exports.create = async function(req, res) {
+  try {
 
-module.exports.update = function(req, res) {}
+  } catch (error) {
+    errorHandler(res, error)
+  }
+}
+
+module.exports.remove = async function(req, res) {
+  try {
+
+  } catch (error) {
+    errorHandler(res, error)
+  }
+}
+
+module.exports.update = async function(req, res) {
+  try {
+
+  } catch (error) {
+    errorHandler(res, error)
+  }
+}
